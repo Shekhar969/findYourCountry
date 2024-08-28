a2fb9c1ae78f33424088ab4e01e507d4fcb5e14a
Shekhar969 <shekharrawal96@gmail.com>
2024-08-28 19:40:07 +0545

    Html done

--- script.js
+++ script.js
@@ -20,16 +20,13 @@ mode.addEventListener("click", () => {
   }
 });
 
+function print(data) {
+  country.innerHTML = ``;
+  for (let i = 0; i < data.length; i++) {
+    let countrys = document.createElement("div");
+    countrys.classList.add("country");
 
-function print(data){
-    country.innerHTML=``;
-    for(let i=0; i<data.length; i++)
-      {
-            let countrys = document.createElement('div');
-            countrys.classList.add("country");
-            
-        
-            countrys.innerHTML=`<img src=${data[i]?.flags?.png} alt="${data[i]?.flags?.alt}">
+    countrys.innerHTML = `<img src=${data[i]?.flags?.png} alt="${data[i]?.flags?.alt}">
             <div class="info"><hr>
             <h3>${data[i]?.name?.common}</h3><hr>
             <div>
@@ -46,25 +43,62 @@ function print(data){
         
             </div>
             `;
-            
-            country.appendChild(countrys);
-            
-          }}
 
-          
-          let response = fetch("https://restcountries.com/v3.1/all");
-          
-          response
-            .then((reply) => {
-              return reply.json();
-            })
-            .then((data) => {
-              console.log(data);
-              // console.log(data[69].name.common)
-              print(data);
-            })
-            .catch((error) => {
-              console.log("Api Not Calling ");
-            });
-          
-          
+    country.appendChild(countrys);
+  }
+}
+
+let response = fetch("https://restcountries.com/v3.1/all");
+
+response
+  .then((reply) => {
+    return reply.json();
+  })
+  .then((data) => {
+    console.log(data);
+    // console.log(data[69].name.common)
+    print(data);
+  })
+  .catch((error) => {
+    console.log("Api Not Calling ");
+  });
+
+
+
+  let search =document.querySelector("input[type='search']")
+
+  search.addEventListener("input", ()=>{
+
+    let searched;
+    if (!search.value ==""){
+      searched=fetch((`https://restcountries.com/v3.1/name/${search.value}`))
+    }else{
+      searched=response;
+    }
+
+    searched.then(response=>{
+      return response.json();
+    }).then(country =>{
+      print(country);
+    }).catch(error=>{
+      console.log("Searching Not Happeing ")
+      log(error)
+    })
+  })
+
+  select.addEventListener('change',()=>{
+
+    // log(select.value);
+    let searchedRegion = fetch(`https://restcountries.com/v3.1/region/${Option.value}`);
+
+    searchedRegion.then(response=>{
+        return response.json();
+    }).then(countryByRegion =>{
+        print(countryByRegion);
+    }).catch(err=>{
+        log(err);
+    })
+
+    search.value = ``;
+})
+
